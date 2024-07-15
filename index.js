a315cb530405b047b543f4b890bb60f7d01763c3
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-07-15 18:55:30 +0530

    added meal by ID

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ let recipedata = {
   instructions: "",
 };
 
+let cardData = {
+  card_thumbnail: "",
+  card_title: "",
+  card_id: "",
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -20,7 +26,7 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/singlereciep", async (req, res) => {
+app.get("/random", async (req, res) => {
   try {
     const response = await axios.get(
       "https://www.themealdb.com/api/json/v1/1/random.php"
@@ -39,13 +45,6 @@ app.get("/singlereciep", async (req, res) => {
 
     //
     response.data.meals.forEach((meal) => {
-      // console.log(
-      //   meal.strMealThumb + "/preview ",
-      //   meal.strMeal,
-      //   ingidentArray,
-      //   meal.strInstructions
-      // );
-
       recipedata = {
         image: meal.strMealThumb,
         title: meal.strMeal,
@@ -53,8 +52,58 @@ app.get("/singlereciep", async (req, res) => {
         instructions: meal.strInstructions,
       };
     });
+    console.log(meal.strMealThumb);
     res.render("singleRecipe.ejs", recipedata);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching the recipe.");
+  }
+});
+
+app.get("/home", async (req, res) => {
+  const response = await axios.get(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+  );
+  console.log(response.data.meals);
+  res.render("home.ejs", { carddata: response.data.meals });
+  // res.sendStatus(200);
+});
+
+app.get("/singlerecipefromid", async (req, res) => {
+  const receipID = req.query.id; //// Get the ID from query parameters
+  console.log(receipID);
+  try {
+    const response = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${receipID}`
+    );
+
+    // generating data
+    const ingidentArray = [];
+    let ingrident = "";
+    response.data.meals.forEach((meal) => {
+      for (var i = 1; i <= 20; i++) {
+        ingrident = meal[`strIngredient${i}`] + " : " + meal[`strMeasure${i}`];
+        if (meal[`strIngredient${i}`] !== "" && meal[`strMeasure${i}`] !== "") {
+          ingidentArray.push(ingrident);
+        }
+      }
+    });
+    response.data.meals.forEach((meal) => {
+      recipedata = {
+        image: meal.strMealThumb,
+        title: meal.strMeal,
+        ingidents: ingidentArray,
+        instructions: meal.strInstructions,
+      };
+    });
+    console.log(recipedata.image);
     // res.sendStatus(200);
+
+    //
+    res.render("singleRecipe.ejs", recipedata);
+
+    // res.status(200).json(response.data); // Send the response data as JSON
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while fetching the recipe.");
--- readme.txt
+++ readme.txt
@@ -27,3 +27,6 @@ www.themealdb.com/api/json/v1/1/filter.php?a=Canadian
 
 
 
+      
+meal api
+https://www.themealdb.com/api.php
